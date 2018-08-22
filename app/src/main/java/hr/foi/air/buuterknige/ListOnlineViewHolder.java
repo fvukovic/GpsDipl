3f10d525a9f3b590b84bae7fc47428501e70df44
bmijac <bmijac@foi.hr>
2018-08-22 18:11:12 +0200

    Geolociranje dva korisnika i izračunavanje udaljenosti

--- app/src/main/java/hr/foi/air/buuterknige/ListOnlineViewHolder.java
+++ app/src/main/java/hr/foi/air/buuterknige/ListOnlineViewHolder.java
@@ -4,11 +4,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
-public class ListOnlineViewHolder extends RecyclerView.ViewHolder{
+public class ListOnlineViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     public TextView txtEmail;
+    ItemClickListener itemClickListener;
 
     public ListOnlineViewHolder(View itemView) {
         super(itemView);
         txtEmail = (TextView)itemView.findViewById(R.id.txt_email);
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition());
+
     }
 }
